c26f267a4c6f0a29b1bbbbec4485b3c7df1a64d3
lopeselio <lopeselio@gmail.com>
2020-01-22 23:40:38 +0530

    changes

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,17 @@
-const Index = () => {
-  return(
-    <h1> I am Index Page </h1>
-  )
+import React from 'react'
+// const Index = () => {
+//   return(
+//     <h1> I am Index Page </h1>
+//   )
+// }
+
+
+class Index extends React.Component{
+
+  render(){
+    return(
+      <h1> I am Index Page from Class Component. Currently in progress</h1>
+    )
+  }
 }
 export default Index;
