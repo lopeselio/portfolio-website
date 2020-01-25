6e7c64343496410a6ffe39a339206741da24e8eb
lopeselio <lopeselio@gmail.com>
2020-01-25 22:59:35 +0530

    created an scss file

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "0432be75e88f49ac808c";
+/******/ 	var hotCurrentHash = "ef817ef49a9ab59eb295";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
