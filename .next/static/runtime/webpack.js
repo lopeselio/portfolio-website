506bb0be8594c724785a513174c5fafd7a26bfaa
lopeselio <lopeselio@gmail.com>
2020-02-26 13:14:11 +0530

    added resources

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "facbcb1bd8d069688d3a";
+/******/ 	var hotCurrentHash = "146241cb535d2573ee3d";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
