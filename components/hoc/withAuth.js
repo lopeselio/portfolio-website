7e3e1c1d74be9a6bc77dce64af2b5d39adff97c6
lopeselio <lopeselio@gmail.com>
2020-04-20 01:30:38 +0530

    baselayout

--- components/hoc/withAuth.js
+++ components/hoc/withAuth.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import BaseLayout from './../components/layouts/BaseLayout'
+
 
 export default function (Component) {
   return class withAuth extends React.Component {
-    render () {
-      return <Component {...this.props} />
+    renderProtectedPage() {
+      const { isAuthenticated } = this.props.auth
+      if (isAuthenticated) {
+        return ( <Component {...this.props} />)
+      }else {
+        return (
+          <BaseLayout {...this.props.auth}>
+            <BasePage>
+              <h1> You are not authenticated. Please Login to gain access.  </h1>
+            </BasePage>
+          </BaseLayout>
+        )
+      }
+    }
+    }
+    render() {
+      return this.renderProtectedPage()
     }
   }
 }
--- pages/secret.js
+++ pages/secret.js
@@ -5,27 +5,6 @@ import withAuth from '../components/hoc/withAuth'
 
 class Secret extends React.Component {
   // renderSecretPage () {
-  //   const { isAuthenticated } = this.props.auth
-  //   if (isAuthenticated) {
-  //     return (
-  //       <BaseLayout {...this.props.auth}>
-  //         <BasePage>
-  //           <h1> I am Secret Page </h1>
-  //           <p> Secret Content Here </p>
-  //         </BasePage>
-  //       </BaseLayout>
-  //     )
-  //   } else {
-  //     return (
-  //       <BaseLayout {...this.props.auth}>
-  //         <BasePage>
-  //           <h1> You are not authenticated. Please Login to gain access.  </h1>
-  //         </BasePage>
-  //       </BaseLayout>
-  //     )
-  //   }
-  // }
-
   render () {
     return (
       <BaseLayout {...this.props.auth}>
