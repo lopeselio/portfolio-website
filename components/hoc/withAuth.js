265640da2609950492e6ef0a41322d335ed3ed5e
lopeselio <lopeselio@gmail.com>
2020-04-20 01:12:00 +0530

    changes

--- components/hoc/withAuth.js
+++ components/hoc/withAuth.js
@@ -2,15 +2,8 @@ import React from 'react'
 
 export default function (Component) {
   return class withAuth extends React.Component {
-    alertMessage () {
-      alert('Some Message!!!!')
-    }
-
     render () {
-      const someVariable1 = '1'
-      const someVariable2 = '2'
-
-      return <Component someVariable1={someVariable1} someVariable2={someVariable2} alertMessage={this.alertMessage} {...this.props} />
+      return <Component {...this.props} />
     }
   }
 }
