09e90cec74563c98a91ecc9447c805234475e928
lopeselio <lopeselio@gmail.com>
2020-04-19 18:14:15 +0530

    secret.js

---
+++ pages/secret.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import BaseLayout from './../components/layouts/BaseLayout'
+import BasePage from '../components/BasePage'
+
+class Secret extends React.Component {
+  renderSecretPage () {
+    const { isAuthenticated } = this.props.auth
+    if (isAuthenticated) {
+      return (
+        <BaseLayout {...this.props.auth}>
+          <BasePage>
+            <h1> I am Secret Page </h1>
+            <p> Secret Content Here </p>
+          </BasePage>
+        </BaseLayout>
+      )
+    } else {
+      return (
+        <BaseLayout {...this.props.auth}>
+          <BasePage>
+            <h1> You are not authenticated. Please Login to gain access.  </h1>
+          </BasePage>
+        </BaseLayout>
+      )
+    }
+  }
+
+  render () {
+    return this.renderSecretPage()
+  }
+}
+
+export default Secret
