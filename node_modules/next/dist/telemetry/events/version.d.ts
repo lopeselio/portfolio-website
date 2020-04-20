26fea6e722a15850109ff117504b41d57af62931
lopeselio <lopeselio@gmail.com>
2020-04-20 16:38:36 +0530

    npm errors

--- node_modules/next/dist/telemetry/events/version.d.ts
+++ node_modules/next/dist/telemetry/events/version.d.ts
@@ -1,3 +1,4 @@
+import { PHASE_DEVELOPMENT_SERVER, PHASE_EXPORT, PHASE_PRODUCTION_BUILD } from '../../next-server/lib/constants';
 declare type EventCliSessionStarted = {
     nextVersion: string;
     nodeVersion: string;
@@ -5,8 +6,13 @@ declare type EventCliSessionStarted = {
     isSrcDir: boolean | null;
     hasNowJson: boolean;
     isCustomServer: boolean | null;
+    hasNextConfig: boolean;
+    buildTarget: string;
+    hasWebpackConfig: boolean;
+    hasBabelConfig: boolean;
 };
-export declare function eventVersion(event: Omit<EventCliSessionStarted, 'nextVersion' | 'nodeVersion'>): {
+declare type NextConfigurationPhase = typeof PHASE_DEVELOPMENT_SERVER | typeof PHASE_PRODUCTION_BUILD | typeof PHASE_EXPORT;
+export declare function eventCliSession(phase: NextConfigurationPhase, dir: string, event: Omit<EventCliSessionStarted, 'nextVersion' | 'nodeVersion' | 'hasNextConfig' | 'buildTarget' | 'hasWebpackConfig' | 'hasBabelConfig'>): {
     eventName: string;
     payload: EventCliSessionStarted;
 }[];
--- server/services/auth.js
+++ server/services/auth.js
@@ -4,6 +4,10 @@ exports.checkJWT = function (req, res, next) {
   const isValidToken = false
 
   if (isValidToken) {
+    res.user = {
+      name: 'Elio',
+      lastname: 'Lopes'
+    }
     next()
   } else {
     return res.status(401).send({ title: 'Not Authorized', detail: 'Please login in order to get our data' })
