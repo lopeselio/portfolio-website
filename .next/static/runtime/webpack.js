92c14bcb67054afe18b65b82eadd10d1389798ce
lopeselio <lopeselio@gmail.com>
2020-04-25 16:39:39 +0530

    HOC

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "44f8841ba810de65ce61";
+/******/ 	var hotCurrentHash = "2db9bdc1318e6ae0224a";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
