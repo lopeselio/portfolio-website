0f52f90565dbbeb5cc928464a3246ef56d5dcc32
lopeselio <lopeselio@gmail.com>
2020-04-26 12:57:14 +0530

    basepage changes

--- .next/static/chunks/styles.js
+++ .next/static/chunks/styles.js
@@ -66,7 +66,7 @@
           .querySelectorAll("link[href][rel=stylesheet]")
           .forEach(function(link) {
             if (!link.href.match(prefix) || link.stale) return;
-            injectCss(link, link.href.split("?")[0] + "?unix=1587885088631");
+            injectCss(link, link.href.split("?")[0] + "?unix=1587886008257");
           });
       }
     }
--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "fffbe39714574658a0c3";
+/******/ 	var hotCurrentHash = "9c1357907eda3d44fb54";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
