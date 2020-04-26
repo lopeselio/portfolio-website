1e87e6bbb7d21510f8e718b9962ea80f3739fa01
lopeselio <lopeselio@gmail.com>
2020-04-26 12:48:22 +0530

    require authentication

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "8dda8cc3cf78024449b8";
+/******/ 	var hotCurrentHash = "fffbe39714574658a0c3";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- pages/portfolioNew.js
+++ pages/portfolioNew.js
@@ -2,12 +2,13 @@ import React from 'react'
 // import Header from './../components/shared/Header'
 import BaseLayout from './../components/layouts/BaseLayout'
 import BasePage from '../components/BasePage'
+import withAuth from '../components/hoc/withAuth'
 
 class PortfolioNew extends React.Component {
   render () {
     return (
       <BaseLayout {...this.props.auth}>
-        <BasePage className='about-page' title='Create New Portfolio'>
+        <BasePage className='portfolio-create-page' title='Create New Portfolio'>
         </BasePage>
         {/* <Header/> */}
       </BaseLayout>
@@ -15,4 +16,4 @@ class PortfolioNew extends React.Component {
   }
 }
 
-export default PortfolioNew
+export default withAuth('siteOwner')(PortfolioNew)
