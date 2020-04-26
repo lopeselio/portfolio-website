dc2f73a361ecec43ce46f9ddea15a17a257d1b86
lopeselio <lopeselio@gmail.com>
2020-04-26 11:09:33 +0530

    changes

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "396dcb985b3ddd6f317e";
+/******/ 	var hotCurrentHash = "d7a1e41deabd1b9c0092";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
